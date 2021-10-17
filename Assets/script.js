40b5525b4a407b3b532596c2651afbb545e269d1
Estefany Silfa <esilfa3@outlook.com>
2021-10-16 20:12:20 -0400

    updates

--- Assets/script.js
+++ Assets/script.js
@@ -12,16 +12,16 @@ for (var i = 0; i < localStorage.length; i++) {
 
     cityName.append("<li>" + city + "</li>");
 }
-// Key count for local storage 
+//  local storage 
 var keyCount = 0;
-// Search button click event
+
 searchButton.click(function () {
 
     var searchInput = $(".searchInput").val();
 
-    // Variable for current weather working 
+    // current weather working 
     var urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
-    // Variable for 5 day forecast working
+    // 5 day forecast working
     var urlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
 
     if(searchInput == "") {
@@ -31,33 +31,30 @@ searchButton.click(function () {
             url: urlCurrent,
             method: "GET"
         }).then(function (response) {
-            // list-group append an li to it with just set text
-            // console.log(response.name);
+            // list-group append 
             var cityName = $(".list-group").addClass("list-group-item");
             cityName.append("<li>" + response.name + "</li>");
             // Local storage
             var local = localStorage.setItem(keyCount, response.name);
             keyCount = keyCount + 1;
 
-            // Start Current Weather append 
+            // Current Weather append 
             var currentCard = $(".currentCard").append("<div>").addClass("card-body");
             currentCard.empty();
             var currentName = currentCard.append("<p>");
-            // .addClass("card-text");
             currentCard.append(currentName);
 
             // Adjust Date 
             var timeUTC = new Date(response.dt * 1000);
             currentName.append(response.name + " " + timeUTC.toLocaleDateString("en-US"));
             currentName.append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
-            // Add Temp 
+    
             var currentTemp = currentName.append("<p>");
-            // .addClass("card-text");
             currentName.append(currentTemp);
             currentTemp.append("<p>" + "Temperature: " + response.main.temp + "</p>");
-            // Add Humidity
+            //  Humidity
             currentTemp.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
-            // // Add Wind Speed: 
+            // // Wind Speed: 
             currentTemp.append("<p>" + "Wind Speed: " + response.wind.speed + "</p>");
 
             // UV Index URL
@@ -68,15 +65,13 @@ searchButton.click(function () {
                 url: urlUV,
                 method: "GET"
             }).then(function (response) {
-
                 var currentUV = currentTemp.append("<p>" + "UV Index: " + response.value + "</p>").addClass("card-text");
                 currentUV.addClass("UV");
                 currentTemp.append(currentUV);
-                // currentUV.append("UV Index: " + response.value);
             });
 
         });
-    // Start call for 5-day forecast 
+    // Call for 5-day forecast 
     $.ajax({
         url: urlFiveDay,
         method: "GET"
@@ -86,7 +81,7 @@ searchButton.click(function () {
         var fiveDayCard = $(".fiveDayCard").addClass("card-body");
         var fiveDayDiv = $(".fiveDayOne").addClass("card-text");
         fiveDayDiv.empty();
-        // For each for 5 days
+        // For each days
         day.forEach(function (i) {
             var FiveDayTimeUTC1 = new Date(response.list[i].dt * 1000);
             FiveDayTimeUTC1 = FiveDayTimeUTC1.toLocaleDateString("en-US");
